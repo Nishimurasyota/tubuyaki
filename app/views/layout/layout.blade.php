b11737247fc52d85cce5ddc83b417eff00bbb8b4
nishimurasyota <syota4723@gmail.com>
2022-04-07 08:59:46 +0900

    :adhesive_bandage: レイアウト用viewファイルの修正

--- app/views/layout/layout.blade.php
+++ app/views/layout/layout.blade.php
@@ -5,10 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <title>Document</title>
+    <title>@yield('title')</title>
 </head>
 
 <body>
+    <div class="header">
+        @yield('header')
+    </div>
+
+    <div class="contents">
+        <h1>@yield('title')</h1>
+
+        <div class="sidebar">
+            @yield("sidebar")
+        </div>
+
+        <div class="mainContent">
+            @yield("mainContent")
+        </div>
+    </div>
+
+
+    <div class="footer">
+        @yield("footer")
+    </div>
+
+
 
 </body>
 
